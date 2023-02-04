96fb4c8bc06a03e18c88f5f44ecdfe34a7e26281
Vishesh Kumar Singh <visheshkrsinghofficial@gmail.com>
2023-02-04 07:27:05 +0530

    saving to json

--- predictor/insights.py
+++ predictor/insights.py
@@ -1,6 +1,7 @@
 from s2t import Speech2Text
 from sentAnal import Analyzer
 import sys
+import json
 
 file_path = sys.argv[1]
 
@@ -10,8 +11,7 @@ def results(file_name):
     transcription = stt.process(file_name)
     print(transcription)    
     analyzer = Analyzer()
-
-    return analyzer.detect_emotion(transcription)
-    
+    with open(f'{file_name}_results.json', 'w') as f:
+        json.dump(analyzer.detect_emotion(transcription), f, indent=4)
 
 results(file_path)
